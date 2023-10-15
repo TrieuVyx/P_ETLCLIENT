09e09819ca9b17eba6a86fd952ef1d5af482dccc
andrewzkn <trieuvyxka1@gmail.com>
2023-10-15 13:27:29 +0700

    react + nodejs source (mongoose)

--- BE/index.js
+++ BE/index.js
@@ -7,22 +7,27 @@ const path = require("path");
 const cors = require("cors")
 
 routers(app)
-//cau hinh file env
+//cấu hình file env
 require('dotenv').config()
+//kết nối db 
 db.connect();
 
 //size file json nhận
-app.use(bodyParser.json({ limit: '50mb' }));
+
+//gửi dữ liệu dạng json, giới hạn dữ liẹu json
+app.use(bodyParser.json({ limit: '50mb' })); 
+//phân tích url gửi dữ liệu json dạng endcode
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+// nhận dữ liệu phức tạp
 app.use(express.urlencoded({ extended: true }));
+//gửi dữ liệu dạng json
 app.use(express.json());
+//phục vụ các tệp file tĩnh 
 app.use(express.static(path.join(__dirname, "public")));
+//xử lí vấn đề các nguồn web truy cập vào và chấp nhận
 app.use(cors({ origin: "*" }));
 
-
-
-
-app.listen(process.env.PORT,() => {
-    console.log(`server is running on port http://${process.env.HOST}:${process.env.PORT}`)
+app.listen(process.env.PORT1,() => {
+    console.log(`server is running on port http://${process.env.HOST1}:${process.env.PORT1}`)
 })
 
